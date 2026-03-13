163a6b7a599d7d65557c75416ffbb27b21bd4ba2
jvbgomes <jvbg16@gmail.com>
2026-03-13 00:09:40 -0300

    att do jogo

--- estudo/advanced/forca.c
+++ estudo/advanced/forca.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
+//Variáveis globais
+char palavrasecret[20];
+char chutes[26];
+int tentativas = 0;
+
 void abertura() {
     printf("***********************\n");
     printf("* Jogo de Forca       *\n");
     printf("***********************\n\n");
 }
 
-void chuta(char chutes[26], int* tentativas) {
+void chuta() {
     char chute;
+    printf("Qual letra? ");
     scanf(" %c", &chute);
 
-    chutes[(*tentativas)] = chute;
-    (*tentativas)++;
+    chutes[tentativas] = chute;
+    tentativas++;
 }
 
-int jachutou(char letra, char chutes[26], int tentativas) {
+int jachutou(char letra) {
     int achou = 0;
     for(int j = 0; j < tentativas; j++) {
         if(chutes[j] == letra) {
@@ -26,36 +32,40 @@ int jachutou(char letra, char chutes[26], int tentativas) {
     return achou;
 }
 
-int main() {
-    char palavrasecret[20];
-    
-    sprintf(palavrasecret, "BANANA");
+void desenhaforca() {
 
+        
+
+    for(int i = 0; i < strlen(palavrasecret); i++) {
+
+        printf("Voce ja deu %d chutes\n", tentativas);
+
+        if(jachutou(palavrasecret[i])) {
+            printf("%c ", palavrasecret[i]);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n"); 
+}
+
+void escolhepalavra() {
+    sprintf(palavrasecret, "MELANCIA");
+
+}
+
+int main() {
+    
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
-
     abertura();
+    escolhepalavra();
 
     do {
-        //imprime a palavra secreta
-        for(int i = 0; i < strlen(palavrasecret); i++) {
-
-            jachutou(palavrasecret[i], chutes, tentativas);
-
-            if(achou) {
-                printf("%c ", palavrasecret[i]);
-            }
-            else {
-                printf("_ ");
-            }
-        }
-        printf("\n"); 
-
-        chuta(chutes, &tentativas); 
         
+        desenhaforca();
+        chuta(); 
 
     } while(!acertou && !enforcou);
 

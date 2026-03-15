3d704b2a22e1fa24ecc393d561e1f6148dfb1e71
jvbgomes <jvbg16@gmail.com>
2026-03-15 04:47:28 -0300

    organizando arquivos

--- estudo/advanced2/mapa.h
+++ estudo/advanced2/mapa.h
@@ -10,3 +10,11 @@ void liberamapa(MAPA* m);
 void alocamapa(MAPA* m);
 void lemapa(MAPA* m);
 void imprimemapa(MAPA* m);
+struct posicao {
+    int x;
+    int y;
+};
+
+typedef struct posicao POSICAO;
+
+void encontramapa(MAPA* m, POSICAO* p, char c);

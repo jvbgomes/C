5bcc9fb7faf8bd4adee961327f63f4424076c60c
jvbgomes <jvbg16@gmail.com>
2026-05-18 23:33:07 -0300

    add: EDB selection sort - O(n^2) com selecao do minimo

---
+++ EDB/selection-sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i) {
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+}
+
+void imprimir_vetor(int arr[], int n) {
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Antes:  ");
+    imprimir_vetor(arr, n);
+
+    selection_sort(arr, n);
+
+    printf("Depois: ");
+    imprimir_vetor(arr, n);
+
+    return 0;
+}
